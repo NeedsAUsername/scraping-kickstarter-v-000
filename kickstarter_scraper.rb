12a4e9adc964bcca568de6e080e6f9fea976506b
Kevin Chi <kevinchi118@gmail.com>
2018-06-26 00:49:51 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,4 +13,4 @@ end
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project")
+# percent_funded: project.css("ul.project-stats ")
